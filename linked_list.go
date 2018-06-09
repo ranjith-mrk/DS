f29ccbc57bb82d9f9b43bd7c1d3a5d5b1929fbd2
ranjith-mrk <innranjith@gmail.com>
2018-06-09 23:58:15 +0530

    Base skeleton

---
+++ linked_list.go
@@ -0,0 +1,44 @@
+package main
+
+// import (
+//   "fmt"
+// )
+
+type List struct {
+  root Node
+}
+
+type Node struct {
+  next *Node
+  value interface{}
+}
+
+
+func (n *Node) init() *List{
+
+}
+
+func (n *Node) insert(e, at *Element) *Element {
+
+}
+
+func (n *Node) insertValue(val interface{}, at *Element) *Element {
+
+}
+
+func (n *Node) insertAtFront(val interface{}) *Element{
+
+}
+
+func (n *Node) insertAtEnd(val interface{}) *Element {
+
+}
+
+func New() *List {
+
+}
+
+func main() {
+  
+}
+
