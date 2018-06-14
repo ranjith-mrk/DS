1e391c3932933694c54b8d78091f85425b15bfa8
ranjith-mrk <innranjith@gmail.com>
2018-06-14 16:29:38 +0530

    Check for git

--- linked_list.go
+++ linked_list.go
@@ -13,17 +13,30 @@ type Node struct {
   value interface{}
 }
 
-
-func (n *Node) init() *List{
-
+func (e *Element) Next() *Element{
+  if node := e.next; e.list != nil && node != &e.list.root{
+    return n
+  }
+  return nil
 }
 
-func (n *Node) insert(e, at *Element) *Element {
 
+func (n *Node) init() *List{
+  l.root.next = &l.root
+  l.len = 0
+  return l
+}
+
+func (n *Node) insert(e, target *Element) *Element {
+  node := target.Next()
+  target.next = e
+  e.next = node
+  l.len = l.len + 1
+  return e
 }
 
 func (n *Node) insertValue(val interface{}, at *Element) *Element {
-
+  
 }
 
 func (n *Node) insertAtFront(val interface{}) *Element{
@@ -39,6 +52,12 @@ func New() *List {
 }
 
 func main() {
-  
+  list := New()
+  node1 := list.insertValue(1, l.root.next)
+  node2 := list.insertValue(2, node1)
+  node3 := list.insertValue(3, node2)
+  node4 := list.insertValue(4, node3)
+  node5 := list.insertValue(5, node4)
+  node6 := list.insertValue(6, node5)
 }
 
